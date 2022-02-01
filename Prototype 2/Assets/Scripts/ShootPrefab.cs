c5afe5bd0ed309e074e058a8487be0aa80c79364
ZachyD555 <zdaly22@gmail.com>
2022-01-31 19:46:35 -0600

    WIP

---
+++ Prototype 2/Assets/Scripts/ShootPrefab.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach to player
+public class ShootPrefab : MonoBehaviour
+{
+    public GameObject prefabToShoot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Instantiate(prefabToShoot, transform.position, prefabToShoot.transform.rotation);
+    }
+}
