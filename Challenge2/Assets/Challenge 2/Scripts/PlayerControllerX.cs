268798129df48d92b3c9b69b4d9852e75b82207e
ZachyD555 <zdaly22@gmail.com>
2022-02-04 21:52:27 -0600

    Added shooting delay
    Challenge 2 is done! 2/4/2022

--- Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -12,12 +12,16 @@ public class PlayerControllerX : MonoBehaviour
     public GameObject dogPrefab;
     public HealthSystemX healthSystem;
 
+    public float fireRate = .75f;
+    private float nextFire = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
